96f7abd28db1d86da59ab5688fb584b0768b42cd
Code-Kate7 <kate.ember99@gmail.com>
2021-06-09 22:01:10 +0200

    Accept and Decline Invitation

---
+++ src/inviteToGroup.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// When the accept button is pressed, the page routes to group chat.
+const Acceptbutton = document.getElementById('Acceptbtn')
+Acceptbutton.addEventListener('click', function myFunction () {
+  window.location = 'chat.html'
+  document.write('Please Wait...Adding you to your new Group...')
+  setTimeout(myFunction(), 4000)
+}, false)
+
+// When the decline button is pressed it routes back to the home-page.
+const button = document.getElementById('Declinebtn')
+button.addEventListener('click', function () {
+  window.location = 'home.html'
+}, false)
