3fc5e074fbfe64e452dfc30a6c866bfefcb66fe7
Code-Kate7 <kate.ember99@gmail.com>
2021-06-17 12:57:10 +0200

    Fixing conflict

--- src/mainRoutes.js
+++ src/mainRoutes.js
@@ -3,26 +3,39 @@
 const path = require('path')
 const express = require('express')
 const mainRouter = express.Router()
+const db = require('../db.js')
 const bcrypt = require('bcrypt')
 const users = []
 
 const passport = require('passport')
 
 const initializePassport = require('./passport-config')
-const { response } = require('express')
 initializePassport(passport, email => users.find(user => user.email === email),
   id => users.find(user => user.id === id)
 )
 
 mainRouter.get('/', function (req, res) {
-  res.send('Hello World, I\'m Node.js. Nice to meet you!')
+  res.render('../views/register.ejs')
 })
 
 mainRouter.get('/home', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'home.html'))
+  mainRouter.get('/public/home.css', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public', '/home.css'))
+  })
 })
 
-mainRouter.get('/register', function (req, res) {
+mainRouter.get('/creategroup', function (req, res) {
+  res.sendFile(path.join(__dirname, '../views', 'createGroupForm.html'))
+  mainRouter.get('/public/form.css', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public', '/form.css'))
+  })
+  mainRouter.get('/src/createGroup.js', function (req, res) {
+    res.sendFile(path.join(__dirname, '../src', '/createGroup.js'))
+  })
+})
+
+mainRouter.get('/register', (req, res) => {
   res.render('../views/register.ejs')
 })
 
@@ -48,7 +61,7 @@ mainRouter.get('/login', function (req, res) {
 
 // Serve html file to js file
 mainRouter.get('/chat', function (req, res) {
-  res.sendFile(path.join(__dirname, '../src', 'chat.html'))
+  res.sendFile(path.join(__dirname, '../views', 'chat.html'))
 })
 
 mainRouter.post('/login', passport.authenticate('local', {
@@ -57,13 +70,30 @@ mainRouter.post('/login', passport.authenticate('local', {
   failureFlash: true
 }))
 
-mainRouter.post('/home', function (req, res) {
-  res.redirect('/chat')
-})
-
 mainRouter.delete('/logout', function (req, res) {
   req.logOut()
   res.redirect('/login')
+})
+
+mainRouter.get('/database', function (req, res) {
+  // Make a query to the database
+  db.pools
+    // Run query
+    .then((pool) => {
+      return pool.request()
+        // This is only a test query, change it to whatever you need
+        .query('SELECT 1')
+    })
+    // Send back the result
+    .then(result => {
+      res.send(result)
+    })
+    // If there's an error, return that with some description
+    .catch(err => {
+      res.send({
+        Error: err
+      })
+    })
 })
 
 module.exports = mainRouter
