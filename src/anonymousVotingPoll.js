9e37bcfb25db54db11cd2b87c69af60934278180
Code-Kate7 <kate.ember99@gmail.com>
2021-06-17 18:29:32 +0200

    Poll complete with tests

--- src/anonymousVotingPoll.js
+++ src/anonymousVotingPoll.js
@@ -1,16 +1,24 @@
 'use strict'
 
-function incrementYesButton () {
-  const element = document.getElementById('num1')
-  let value = element.innerHTML
-  ++value
-
-  document.getElementById('num1').innerHTML = value
+// Setting the value of the heading to 1 + existing value.
+// For when yes button is pressed.
+function setValue1 () {
+  const element1 = document.getElementById('num1')
+  const value1 = element1.innerHTML
+  document.getElementById('num1').innerHTML = incrementYesButton(value1)
 }
-function incrementNoButton () {
-  const element = document.getElementById('num2')
-  let value = element.innerHTML
-  ++value
-
-  document.getElementById('num2').innerHTML = value
+// For when no button is pressed.
+function setValue2 () {
+  const element2 = document.getElementById('num2')
+  const value2 = element2.innerHTML
+  document.getElementById('num2').innerHTML = incrementNoButton(value2)
 }
+
+// Incrementation for each button
+function incrementYesButton (value1) {
+  return ++value1
+}
+function incrementNoButton (value2) {
+  return ++value2
+}
+module.exports = incrementYesButton
