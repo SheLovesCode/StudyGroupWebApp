4bab43da7ae00d21040b200b87602d7c45c93716
Steven <stevengmakoni2@gmail.com>
2021-06-21 15:30:50 +0200

    Update the User Table columns with Username and Address Columns

--- src/database/CreateTables.sql
+++ src/database/CreateTables.sql
@@ -1,11 +1,13 @@
 ---Create Tables
-
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users (
-    id STRING NOT NULL UNIQUE,
-    name varchar(255) NOT NULL UNIQUE,
+    id int identity NOT NULL,
+    username varchar(255) NOT NULL UNIQUE,
+   -- firstName varchar(255) NOT NULL,
+   -- lastName varchar(255) NOT NULL,
     email varchar(255) NOT NULL UNIQUE,
     password varchar(255) NOT NULL,
-    PRIMARY KEY (id)
+	address varchar(255) NOT NULL,
+    PRIMARY KEY(username)
 );
