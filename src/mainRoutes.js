807051e76bcd826b5b2de015c4050ae0e93cd445
SheLovesCode <munyaradzi.diana@gmail.com>
2021-06-23 19:40:33 +0200

    Fixed Covid Form Rerouting

--- src/mainRoutes.js
+++ src/mainRoutes.js
@@ -11,89 +11,95 @@ const passport = require('passport')
 
 const initializePassport = require('./passport-config')
 initializePassport(passport, email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
+    id => users.find(user => user.id === id)
 )
 
-mainRouter.get('/', function (req, res) {
-  res.render('../views/register.ejs')
+mainRouter.get('/', function(req, res) {
+    res.render('../views/register.ejs')
 })
 
-mainRouter.get('/home', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'home.html'))
-  mainRouter.get('/public/home.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/home.css'))
-  })
+mainRouter.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'home.html'))
+    mainRouter.get('/public/home.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/home.css'))
+    })
 })
 
-mainRouter.get('/creategroup', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'createGroupForm.html'))
-  mainRouter.get('/public/form.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/form.css'))
-  })
-  mainRouter.get('/src/createGroup.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src', '/createGroup.js'))
-  })
+mainRouter.get('/creategroup', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'createGroupForm.html'))
+    mainRouter.get('/public/form.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/form.css'))
+    })
+    mainRouter.get('/src/createGroup.js', function(req, res) {
+        res.sendFile(path.join(__dirname, '../src', '/createGroup.js'))
+    })
 })
 
 mainRouter.get('/register', (req, res) => {
-  res.render('../views/register.ejs')
+    res.render('../views/register.ejs')
 })
 
-mainRouter.post('/register', async function (req, res) {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    })
-    res.redirect('/login')
-  } catch {
-    res.redirect('/register')
-  }
-  console.log(users)
+mainRouter.post('/register', async function(req, res) {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users)
 })
 
-mainRouter.get('/login', function (req, res) {
-  res.render('../views/login.ejs')
+mainRouter.get('/login', function(req, res) {
+    res.render('../views/login.ejs')
 })
 
 // Serve html file to js file
-mainRouter.get('/chat', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'chat.html'))
+mainRouter.get('/chat', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'chat.html'))
 })
 
 mainRouter.post('/login', passport.authenticate('local', {
-  successRedirect: '/home',
-  failureRedirect: '/login',
-  failureFlash: true
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true
 }))
 
-mainRouter.delete('/logout', function (req, res) {
-  req.logOut()
-  res.redirect('/login')
+// Covid Screening after invitation
+mainRouter.get('/CovidScreening', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'covidForm.html'))
 })
 
-mainRouter.get('/database', function (req, res) {
-  // Make a query to the database
-  db.pools
-    // Run query
-    .then((pool) => {
-      return pool.request()
-        // This is only a test query, change it to whatever you need
-        .query('SELECT 1')
-    })
-    // Send back the result
-    .then(result => {
-      res.send(result)
-    })
-    // If there's an error, return that with some description
-    .catch(err => {
-      res.send({
-        Error: err
-      })
-    })
+
+mainRouter.delete('/logout', function(req, res) {
+    req.logOut()
+    res.redirect('/login')
+})
+
+mainRouter.get('/database', function(req, res) {
+    // Make a query to the database
+    db.pools
+        // Run query
+        .then((pool) => {
+            return pool.request()
+                // This is only a test query, change it to whatever you need
+                .query('SELECT 1')
+        })
+        // Send back the result
+        .then(result => {
+            res.send(result)
+        })
+        // If there's an error, return that with some description
+        .catch(err => {
+            res.send({
+                Error: err
+            })
+        })
 })
 
 module.exports = mainRouter
