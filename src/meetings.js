42b06af44449b230b0f2c9abfa560bdb69d96527
SheLovesCode <munyaradzi.diana@gmail.com>
2021-06-24 18:16:10 +0200

    Added Send Online Meeting Invite Functionality

--- src/meetings.js
+++ src/meetings.js
@@ -1,1 +1,6 @@
-document.getElementById('sendOnlineInvite');
+const modalButton = document.querySelector('.meetingBtn');
+const modalBackground = document.querySelector('.backgroundModal')
+
+modalButton.addEventListener('click', function() {
+    modalBackground.classList.add('popupActive');
+});
