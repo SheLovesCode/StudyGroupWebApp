f40145870f36e6ac960dee6036925c304f8b516c
SheLovesCode <munyaradzi.diana@gmail.com>
2021-06-30 15:12:59 +0200

    Enter and details for meeting

--- src/meetings.js
+++ src/meetings.js
@@ -1,3 +1,23 @@
+let meetingInformation = document.getElementsByClassName("faceMeeting");
+
 document.getElementById("faceMeetingBtn").onclick = function() {
-    console.log("You're sending the invite");
+
+    // Creating meeting div
+    let mainContainer = document.getElementById('mainContainer');
+    let newMeeting = document.createElement('div');
+    newMeeting.id = "faceToFaceDiv";
+    newMeeting.className = "meetingDivs"; // Will be used to style div
+    newMeeting.innerText = "Date: " + meetingInformation[0].value + " at " + meetingInformation[1].value + "\n";
+    mainContainer.appendChild(newMeeting);
+
+    // Add paragraph to the div
+    let instructions = document.createElement('p');
+    instructions.innerHTML = "Please complete the COVID-19 form to Accept/Decline this meeting invite."
+    newMeeting.appendChild(instructions);
+
+    // Add a button to that div so users can be taken to the Covid Form
+    let covidFormBtn = document.createElement('button');
+    covidFormBtn.id = "getCovidForm";
+    covidFormBtn.innerHTML = "Complete COVID-19 Screening Form";
+    newMeeting.appendChild(covidFormBtn);
 }
