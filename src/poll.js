2319f64b9f6e358271313b3e337e2fbf04deeb1a
Code-Kate7 <kate.ember99@gmail.com>
2021-06-30 14:35:44 +0200

    Create termination poll functionality done!

--- src/poll.js
+++ src/poll.js
@@ -1,5 +1,23 @@
 'use strict'
 
+// When Create Poll button is pressed, a "Successful Creation of Termination Poll" message must be sent .
+const createTerminationPollButton = document.getElementById('createPollbtn')
+createTerminationPollButton.addEventListener('click', function myFunction () {
+  const Username = document.querySelector('#myUsername').value
+  const Reason = document.querySelector('#myReason').value
+
+  informationChecker(Username, Reason)
+}, false)
+
+function informationChecker (Username, Reason) {
+  if (Username === '' || Reason === '') {
+    alert('Please enter all information')
+  } else {
+    alert('Termination poll regarding ' + Username + ' successfully created for this reason: ' + Reason)
+    document.forms[0].reset()
+  }
+}
+
 // When View Pending Application button is pressed, the page routes to Pending applications.
 const viewApplicationbutton = document.getElementById('Applicationbtn')
 viewApplicationbutton.addEventListener('click', function myFunction () {
