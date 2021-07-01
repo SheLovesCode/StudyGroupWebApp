17f25a143321af7ab0f6fd27f519b412ebe495db
SheLovesCode <munyaradzi.diana@gmail.com>
2021-07-01 12:51:27 +0200

    Add accept/decline modal

--- src/covidForm.js
+++ src/covidForm.js
@@ -1,20 +1,5 @@
 function generateAcceptDeclineBtns(allowedToMeet) {
-    // Extracting meeting div
-    let meetingDiv = document.getElementById("faceToFaceDiv");
 
-    // Add an accept button to the meeting div
-    let acceptBtn = document.createElement('button');
-    acceptBtn.id = "acceptBtn";
-    acceptBtn.innerHTML = "Accept Invitation";
-    meetingDiv.append(acceptBtn);
-
-    // Add a decline button to the meeting div
-    let declineBtn = document.createElement('button');
-    declineBtn.id = "declineBtn";
-    declineBtn.innerHTML = "Decline Invitation";
-    meetingDiv.append(declineBtn);
-
-    // Display warning about co-morbidities etc
     console.log("Accepting or declining");
 }
 
