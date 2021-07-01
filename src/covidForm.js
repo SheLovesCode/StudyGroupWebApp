f50d6250ab9aa9bb7fde4fde2f5e7035b7838f41
SheLovesCode <munyaradzi.diana@gmail.com>
2021-07-01 14:09:47 +0200

    Created modal with Accept/Decline buttons

--- src/covidForm.js
+++ src/covidForm.js
@@ -1,4 +1,31 @@
 function generateAcceptDeclineBtns(allowedToMeet) {
+    // People who are allowed to meet in person who aren't high risk
+    if (allowedToMeet == "YES") {
+        document.querySelector("#allowedModal").style.display = "flex";
+    }
+    // High risk persons
+    else if (allowedToMeet == "MAYBE") {
+        let covidModalContent = document.getElementById('allowedModalContent');
+        let warningText = document.createElement('p');
+        warningText.id = "warningText";
+        warningText.innerHTML = "Although you're allowed to meet your group members in person, you're an at risk person. Do not forget to be extra cautious."
+        covidModalContent.appendChild(warningText);
+        document.querySelector("#allowedModal").style.display = "flex";
+    }
+    // Not allowed to meet
+    else if (allowedToMeet == "NO") {
+        document.querySelector("#notAllowedModal").style.display = "flex";
+    }
+
+    // Close the modal if the close button is clicked
+    document.getElementById("allowedCloseBtn").onclick = function() {
+        document.getElementById("allowedModal").style.display = "none";
+    }
+
+    // Close the modal if the close button is clicked
+    document.getElementById("notAllowedCloseBtn").onclick = function() {
+        document.getElementById("notAllowedModal").style.display = "none";
+    }
 
     console.log("Accepting or declining");
 }
@@ -21,7 +48,7 @@ function covidScreening() {
     } else if ((C1 == "YES" || C2 == "YES") && (A1 == "NO" && A2 == "NO" && B1 == "NO" && B2 == "NO")) {
         allowedToMeet = "MAYBE";
     } else {
-        allowedToMeet = "YES";
+        allowedToMeet = "NO";
     }
 
     // Create accept/decline functions
