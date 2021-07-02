f43d001e97ff16a93e226e96b8f852416d607175
SheLovesCode <munyaradzi.diana@gmail.com>
2021-07-02 11:00:32 +0200

    Store created invitation in the database

--- src/mainRoutes.js
+++ src/mainRoutes.js
@@ -7,174 +7,187 @@ const db = require('../db.js')
 const accountManager = require('../src/database/dbAccountManagement.js')
 const alert = require('alert')
 const groupManager = require('../src/createGroup')
- 
+const dbMeetings = require('../src/database/dbMeetings')
+
 function checkIfSignedIn(req, res, next) {
-  if (req.session.user) {
-    console.log('There')
-    console.log(req.session.user)
-    console.log('There')
-    next()
-  } else {
-    // const err = new Error('Not logged in')
-    // next(err)
-    alert('User not logged in!')
-    res.redirect('/login')
-  }
+    if (req.session.user) {
+        console.log('There')
+        console.log(req.session.user)
+        console.log('There')
+        next()
+    } else {
+        // const err = new Error('Not logged in')
+        // next(err)
+        alert('User not logged in!')
+        res.redirect('/login')
+    }
 }
 
-mainRouter.get('/', function (req, res) {
-  res.render('../views/register.ejs')
+mainRouter.get('/', function(req, res) {
+    res.render('../views/register.ejs')
 })
 
-mainRouter.get('/home', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'home.html'))
-  mainRouter.get('/public/home.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/home.css'))
-  })
+mainRouter.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'home.html'))
+    mainRouter.get('/public/home.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/home.css'))
+    })
 })
 
-mainRouter.get('/creategroup', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'createGroupForm.html'))
-  mainRouter.get('/public/form.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/form.css'))
-  })
+mainRouter.get('/creategroup', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'createGroupForm.html'))
+    mainRouter.get('/public/form.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/form.css'))
+    })
 })
 
-mainRouter.post('/creategroup', function (req, res) {
-  groupManager.obtainExistingGroups()
-  groupManager.addGroup(req.body, req, res)
+mainRouter.post('/creategroup', function(req, res) {
+    groupManager.obtainExistingGroups()
+    groupManager.addGroup(req.body, req, res)
 })
 
-mainRouter.get('/group', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'grouppage.html'))
-  mainRouter.get('/public/page.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/page.css'))
-  })
-  mainRouter.get('/utils/hello-and-hi.jpg', function (req, res) {
-    res.sendFile(path.join(__dirname, '../utils', '/hello-and-hi.jpg'))
-  })
+mainRouter.get('/group', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'grouppage.html'))
+    mainRouter.get('/public/page.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/page.css'))
+    })
+    mainRouter.get('/utils/hello-and-hi.jpg', function(req, res) {
+        res.sendFile(path.join(__dirname, '../utils', '/hello-and-hi.jpg'))
+    })
 })
 
-mainRouter.get('/group/content', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'insidegroups.html'))
-  mainRouter.get('/group/team.html', function (req, res) {
-    res.sendFile(path.join(__dirname, '../views', '/team.html'))
-  })
-  mainRouter.get('/group/insidegroups.html', function (req, res) {
-    res.sendFile(path.join(__dirname, '../views', '/insidegroups.html'))
-  })
-  mainRouter.get('/group/classNotes.html', function (req, res) {
-    res.sendFile(path.join(__dirname, '../views', '/classNotes.html'))
-  })
-  mainRouter.get('/group/files.html', function (req, res) {
-    res.sendFile(path.join(__dirname, '../views', '/files.html'))
-  })
-  mainRouter.get('/group/poll.html', function (req, res) {
-    res.sendFile(path.join(__dirname, '../views', '/poll.html'))
-  })
-  mainRouter.get('/src/poll.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src', '/poll.js'))
-  })
-  mainRouter.get('/public/insideGroups.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/insideGroups.css'))
-  })
-  mainRouter.get('/utils/hello-and-hi.jpg', function (req, res) {
-    res.sendFile(path.join(__dirname, '../utils', '/hello-and-hi.jpg'))
-  })
+mainRouter.get('/group/content', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'insidegroups.html'))
+    mainRouter.get('/group/team.html', function(req, res) {
+        res.sendFile(path.join(__dirname, '../views', '/team.html'))
+    })
+    mainRouter.get('/group/insidegroups.html', function(req, res) {
+        res.sendFile(path.join(__dirname, '../views', '/insidegroups.html'))
+    })
+    mainRouter.get('/group/classNotes.html', function(req, res) {
+        res.sendFile(path.join(__dirname, '../views', '/classNotes.html'))
+    })
+    mainRouter.get('/group/files.html', function(req, res) {
+        res.sendFile(path.join(__dirname, '../views', '/files.html'))
+    })
+    mainRouter.get('/group/poll.html', function(req, res) {
+        res.sendFile(path.join(__dirname, '../views', '/poll.html'))
+    })
+    mainRouter.get('/src/poll.js', function(req, res) {
+        res.sendFile(path.join(__dirname, '../src', '/poll.js'))
+    })
+    mainRouter.get('/public/insideGroups.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/insideGroups.css'))
+    })
+    mainRouter.get('/utils/hello-and-hi.jpg', function(req, res) {
+        res.sendFile(path.join(__dirname, '../utils', '/hello-and-hi.jpg'))
+    })
 })
 
-mainRouter.get('/sendInvite', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'sendInvite.html'))
-  mainRouter.get('/public/form.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/form.css'))
-  })
-  mainRouter.get('/src/sendInvite.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src', '/sendInvite.js'))
-  })
+mainRouter.get('/sendInvite', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'sendInvite.html'))
+    mainRouter.get('/public/form.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/form.css'))
+    })
+    mainRouter.get('/src/sendInvite.js', function(req, res) {
+        res.sendFile(path.join(__dirname, '../src', '/sendInvite.js'))
+    })
 })
 
-mainRouter.get('/group/applicationPoll', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'applicationPoll.html'))
-  mainRouter.get('/public/votingStyle.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/votingStyle.css'))
-  })
-  mainRouter.get('/src/applicationPoll.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src', '/applicationPoll.js'))
-  })
+mainRouter.get('/group/applicationPoll', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'applicationPoll.html'))
+    mainRouter.get('/public/votingStyle.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/votingStyle.css'))
+    })
+    mainRouter.get('/src/applicationPoll.js', function(req, res) {
+        res.sendFile(path.join(__dirname, '../src', '/applicationPoll.js'))
+    })
 })
 
-mainRouter.get('/group/terminationPoll', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'terminationPoll.html'))
-  mainRouter.get('/public/votingStyle.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/votingStyle.css'))
-  })
-  mainRouter.get('/src/terminationPoll.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src', '/terminationPoll.js'))
-  })
+mainRouter.get('/group/terminationPoll', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'terminationPoll.html'))
+    mainRouter.get('/public/votingStyle.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/votingStyle.css'))
+    })
+    mainRouter.get('/src/terminationPoll.js', function(req, res) {
+        res.sendFile(path.join(__dirname, '../src', '/terminationPoll.js'))
+    })
 })
 
 mainRouter.get('/register', (req, res) => {
-  res.render('../views/register.ejs')
+    res.render('../views/register.ejs')
 })
 
-mainRouter.post('/register', async function (req, res) {
-  console.log(req.body)
-  accountManager.addUser(req.body, req, res)
+mainRouter.post('/register', async function(req, res) {
+    console.log(req.body)
+    accountManager.addUser(req.body, req, res)
 })
 
-mainRouter.get('/profile', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'profile.html'))
-  mainRouter.get('/public/profile.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', '/profile.css'))
-  })
-  mainRouter.get('/src/profile.js', checkIfSignedIn, function (req, res) {
-    res.sendFile(path.join(__dirname, '../src', '/profile.js'))
-  })
+mainRouter.get('/profile', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'profile.html'))
+    mainRouter.get('/public/profile.css', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public', '/profile.css'))
+    })
+    mainRouter.get('/src/profile.js', checkIfSignedIn, function(req, res) {
+        res.sendFile(path.join(__dirname, '../src', '/profile.js'))
+    })
 })
 
-mainRouter.get('/login', function (req, res) {
-  console.log(req.body)
-  res.render('../views/login.ejs')
+mainRouter.get('/login', function(req, res) {
+    console.log(req.body)
+    res.render('../views/login.ejs')
 })
 
 // Serve html file to js file
-mainRouter.get('/chat', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'chat.html'))
+mainRouter.get('/chat', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'chat.html'))
 })
 
-mainRouter.post('/login', function (req, res) {
-  accountManager.login(req.body, req, res)
+mainRouter.post('/login', function(req, res) {
+    accountManager.login(req.body, req, res)
 })
 
-mainRouter.delete('/logout', checkIfSignedIn, function (req, res) {
-  req.session.destroy(function () { })
-  res.redirect('/login')
+mainRouter.delete('/logout', checkIfSignedIn, function(req, res) {
+    req.session.destroy(function() {})
+    res.redirect('/login')
 })
 
 // Covid Screening after invitation
-mainRouter.get('/CovidScreening', function (req, res) {
-  res.sendFile(path.join(__dirname, '../views', 'covidForm.html'))
+mainRouter.get('/CovidScreening', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'covidForm.html'))
 })
 
-mainRouter.get('/database', function (req, res) {
-  // Make a query to the database
-  db.pools
-    // Run query
-    .then((pool) => {
-      return pool.request()
-        // This is only a test query, change it to whatever you need
-        .query('SELECT 1')
-    })
-    // Send back the result
-    .then(result => {
-      res.send(result)
-    })
-    // If there's an error, return that with some description
-    .catch(err => {
-      res.send({
-        Error: err
-      })
-    })
+mainRouter.post('/meetingDetails', (req, res) => {
+    console.log("Username: ", req.sesssion.user.username);
+
+    console.log(req.body.status);
+    if (req.body.status === "Read") {
+        dbMeetings.getMeetingDetails(req, res);
+    }
+    if (req.body.status === "Update") {
+        dbMeetings.storeMeetingDetails(req.body, req, res);
+    }
+})
+
+mainRouter.get('/database', function(req, res) {
+    // Make a query to the database
+    db.pools
+        // Run query
+        .then((pool) => {
+            return pool.request()
+                // This is only a test query, change it to whatever you need
+                .query('SELECT 1')
+        })
+        // Send back the result
+        .then(result => {
+            res.send(result)
+        })
+        // If there's an error, return that with some description
+        .catch(err => {
+            res.send({
+                Error: err
+            })
+        })
 })
 
 module.exports = mainRouter
--- src/meetings.js
+++ src/meetings.js
@@ -1,10 +1,55 @@
+// Get meetings that are already in the database
+const mainContainer = document.getElementById('mainContainer');
+var storedMeetingDetails = [];
+var responseContainer = [];
+var userEmail = "munyaradzi.diana@gmail.com";
+let currentGroupName = "Trash";
+
+
+getfromDB().then(response => {
+    responseContainer = response;
+    displayStoredMeetings(responseContainer);
+})
+
+function displayStoredMeetings(responseContainer) {
+    responseContainer.forEach(element => {
+        const obj = {
+            groupName: element.groupname,
+            date: element.datetime,
+            time: element.time,
+            covidFormUrl: element.url
+        }
+
+        if (currentGroupName === element.groupname) {
+
+            const storedDate = element.datetime;
+            const storedTime = element.time;
+            const storedUrl = element.url;
+
+            // Creating meeting div
+            let newMeeting = document.createElement('div');
+            newMeeting.id = "faceToFaceDiv";
+            newMeeting.className = "meetingDivs"; // Will be used to style div
+            newMeeting.innerText = "Date: " + storedDate + " at " + storedTime + "\n";
+            mainContainer.appendChild(newMeeting);
+
+            var covidFormLink = document.createElement('a');
+            var covidFormLinkText = document.createTextNode("Complete COVID-19 Screening Form");
+            covidFormLink.appendChild(covidFormLinkText);
+            covidFormLink.title = "COVID Screening Form";
+            covidFormLink.href = storedUrl;
+            newMeeting.appendChild(covidFormLink);
+        }
+
+    })
+}
+
 // When the button is clicked, display the modal
 document.getElementById("sendFaceInviteBtn").onclick = function() {
     document.querySelector(".modal").style.display = "flex";
 }
 
 // Extract the div that contains the main_container so we can append to that instead of the body
-const mainContainer = document.getElementById('mainContainer');
 const meetingDate = document.getElementById("meetingDate");
 const meetingTime = document.getElementById("meetingTime");
 
@@ -19,42 +64,31 @@ document.getElementById("faceMeetingBtn").onclick = function() {
     newMeeting.innerText = "Date: " + meetingDate.value + " at " + meetingTime.value + "\n";
     mainContainer.appendChild(newMeeting);
 
-    // storingInDB().then(response => {
-    //     myGroupMembers = response
-    //     console.log(myGroupMembers)
-    // })
-
-    // async function storingInDB(userEmail, group, formUrl, meetingDate, meetingTime) {
-    //     const text = {
-    //         member: userEmail,
-    //         groupName: group,
-    //         url: formUrl,
-    //         dateTime: meetingDate,
-    //         time: meetingTime
-    //     }
-    //     const options = {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(text)
-    //     }
-    //     const response = await fetch('/meetingDetails', options)
-    //     return response.json()
-    // }
+    async function storingInDB(userEmail, group, formUrl, meetingDate, meetingTime) {
+        const text = {
+            member: userEmail,
+            groupName: group,
+            url: formUrl,
+            dateTime: meetingDate,
+            time: meetingTime,
+            status: "Update"
+        }
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(text)
+        }
+        const response = await fetch('/meetingDetails', options)
+        console.log(response);
+    }
 
     // Add paragraph to the div
     let instructions = document.createElement('p');
     instructions.innerHTML = "Please complete the COVID-19 form to Accept/Decline this meeting invite."
     newMeeting.appendChild(instructions);
 
-    // // Add a button to that div so users can be taken to the Covid Form
-    // var covidFormBtn = document.createElement('button');
-    // covidFormBtn.id = "getCovidForm";
-    // covidFormBtn.innerHTML = "Complete COVID-19 Screening Form";
-    // newMeeting.appendChild(covidFormBtn);
-
-    // Add a button to that div so users can be taken to the Covid Form
     var covidFormLink = document.createElement('a');
     var covidFormLinkText = document.createTextNode("Complete COVID-19 Screening Form");
     covidFormLink.appendChild(covidFormLinkText);
@@ -62,15 +96,27 @@ document.getElementById("faceMeetingBtn").onclick = function() {
     covidFormLink.href = '../CovidScreening';
     newMeeting.appendChild(covidFormLink);
 
-    // // When the student opts to fill in the Covid Form
-    // document.getElementById("getCovidForm").onclick = function() {
-    //     console.log("GET COVID FORM")
-    //     window.location.href = '../CovidScreening';
-
-    // }
+    console.log(covidFormLink.href);
+    storingInDB("Diana@gmail.com", "WITS2020", covidFormLink.href, meetingDate.value, meetingTime.value);
+    getfromDB();
 }
 
 // Close the modal if the close button is clicked
 document.getElementById("faceMeetingModalBtn").onclick = function() {
     document.querySelector(".modal").style.display = "none";
+}
+
+async function getfromDB() {
+    const text = {
+        status: "Read"
+    }
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(text)
+    }
+    const response = await fetch('/meetingDetails', options)
+    return response.json();
 }
